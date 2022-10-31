bddafc2e6a548110a42c0708664f17fca4fff6bd
Robinkhilery960 <robinkhilery960@gmail.com>
2022-10-31 22:05:25 +0530

    first commit

---
+++ src/components/Icon.js
@@ -0,0 +1,14 @@
+ import {FaTimes,FaRegCircle,FaPen} from 'react-icons/fa'
+function Icon({name}) {
+    switch(name){
+        case "circle":
+           return <FaRegCircle className="icon"/>
+       case "cross":
+            return <FaTimes className="icon"/>
+       default:
+            return <FaPen className="icon"/> 
+   }
+    
+}
+
+export default Icon
